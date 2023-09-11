c9c9c9f848245ff85598694049ac890bbaa37914
Adam0Brien <adamobrieniot@gmail.com>
2023-09-11 12:29:04 +0100

    src: Add variables and print statements

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,21 @@
 
 fun main(args: Array<String>){
+    val doubleNumber: Double = 1200.45
+    val floatNumber: Float = 700.78f
+    val longNumber: Long = 98765432
+    val intNumber: Int = 123456
+    val shortNumber: Short = 345
+    val byteNumber: Byte = 127
+
     println("Hello Workd !")
     println("Working with Variables and Types")
+
+
+    println("doubleNumber value is: " + doubleNumber + ", and the type is: " + doubleNumber.javaClass)
+    println("floatNumber value is:  " + floatNumber + ", and the type is: " + floatNumber.javaClass)
+    println("longNumber value is:   " + longNumber + ", and the type is: " + longNumber.javaClass)
+    println("intNumber value is:    " + intNumber + ", and the type is: " + intNumber.javaClass)
+    println("shortNumber value is:  " + shortNumber + ", and the type is: " + shortNumber.javaClass)
+    println("byteNumber value is:   " + byteNumber + ", and the type is: " + byteNumber.javaClass)
+
 }
